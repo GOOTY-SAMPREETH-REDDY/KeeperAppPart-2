b64ff24a68c4945503f950abcbb9908e391f3abe
GOOTY-SAMPREETH-REDDY <109329775+GOOTY-SAMPREETH-REDDY@users.noreply.github.com>
2023-06-22 13:35:10 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+function fun(obj) {
+  return <Note id={obj.key} title={obj.title} content={obj.content} />;
+}
+function App() {
+  return (
+    <div>
+      <Header />
+
+      {notes.map(fun)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
